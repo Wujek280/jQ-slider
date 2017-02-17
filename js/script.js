7a63eb4497d0450f749faec0e788730b7fad7d5e
Wujek <mjwojcik94@gmail.com>
2017-02-17 23:15:10 +0100

    nwm

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ var slideCount = $(slideshow).children.length;
 var slideWidth = 100/slideCount;
 var slideIndex = 0;
 
-slideshow.css.width = slideCount*100;
+console.log(slider);
+console.log(slideshow);
+console.log("n :"+slideCount);
+console.log("Width :"+slideWidth);
+console.log("Index : "+slideIndex);
+
+//slideshow.css.width = slideCount*100;
 
 
 
