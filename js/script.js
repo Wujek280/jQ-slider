1b65d6b39e8eb796d4302a33db3917b43d18fc94
Wujek <mjwojcik94@gmail.com>
2017-02-17 02:20:16 +0100

    slides

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var slider = $('#slider');
+var slideshow = $('.slide-show');
+var slideCount = $(slideshow).children.length;
+var slideWidth = 100/slideCount;
+var slideIndex = 0;
+
+slideshow.css.width = slideCount*100;
+
+
+
+
